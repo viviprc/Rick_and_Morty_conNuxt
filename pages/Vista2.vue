03b57d73b869f5c15c577485c485f0ab034a60de
unknown <vivi_reyes_c@hotmail.com>
2020-10-26 17:51:31 -0300

    first commit

---
+++ pages/Vista2.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <img
+        src="/v.png"
+        alt="Vuetify.js"
+        class="mb-5"
+      >
+      <blockquote class="blockquote">
+        &#8220;Vista 2&#8221;
+        <footer>
+        </footer>
+      </blockquote>
+    </v-col>
+  </v-row>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-app>
+    <v-main class="pt-0">
+      <v-container>
+        <v-row>
+          <v-col md="4" v-for="(personaje, i) in personajes" :key="i">
+            <v-card class="mx-auto" max-width="300">
+              <v-img
+                class="white--text align-end"
+                height="200px"
+                :src="personaje.imagen"
+              >
+              </v-img>
+
+              <v-card-title class="pb-0"> {{ personaje.nombre }} </v-card-title>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      personajes: [
+        {
+          id: 1,
+          nombre: 'Morty',
+          imagen: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg',
+        },
+        {
+          id: 2,
+          nombre: 'Rick',
+          imagen: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+        },
+        {
+          id: 3,
+          nombre: 'Summer',
+          imagen: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg',
+        },
+        {
+          id: 4,
+          nombre: 'Beth',
+          imagen: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg',
+        },
+        {
+          id: 5,
+          nombre: 'Jerry',
+          imagen: 'https://rickandmortyapi.com/api/character/avatar/5.jpeg',
+        },
+      ],
+    }
+  },
+}
+</script>
